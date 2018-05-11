d19a2611aadf37a58bd39fbbfae51f75bcbe96cf
Cornelius Ukena <guy@cornelius.dev>
2018-05-10 21:51:42 -0500

    refactor build config and ditch sourcemaps for dist

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,6 +1,4 @@
-const path = require('path');
-const postcss = require('postcss');
-const autoprefixer = require('autoprefixer');
+const config = require('./svelte.config');
 const svelte = require('rollup-plugin-svelte');
 
 module.exports = {
@@ -12,26 +10,6 @@ module.exports = {
         sourcemap: true
     },
     plugins: [
-        svelte({
-            extensions: ['.svlt', '.svelte'],
-            preprocess: {
-                style: async ({content, filename}) => {
-                    const result = await postcss([
-                        autoprefixer({
-                            flexbox: 'no-2009',
-                            grid: true
-                        })
-                    ]).process(content, {
-                        to: path.basename(filename),
-                        from: path.basename(filename)
-                    });
-
-                    return {
-                        code: result.css,
-                        map: result.map
-                    };
-                }
-            }
-        })
+        svelte(config)
     ]
 };
---
+++ svelte.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    extensions: ['.svlt', '.svelte'],
+    preprocess: {
+        style: async ({content, filename}) => {
+            const result = await postcss([
+                autoprefixer({
+                    flexbox: 'no-2009',
+                    grid: true
+                })
+            ]).process(content, {
+                to: path.basename(filename),
+                from: path.basename(filename)
+            });
+
+            return {
+                code: result.css,
+                map: result.map
+            };
+        }
+    }
+};
