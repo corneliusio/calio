f44bdfc08ec87d63a8c0ae03cda4858df5767284
Cornelius Ukena <guy@cornelius.dev>
2019-04-24 10:26:59 -0500

    WIP

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -3,17 +3,31 @@ const svelte = require('rollup-plugin-svelte');
 const common = require('rollup-plugin-commonjs');
 const resolve = require('rollup-plugin-node-resolve');
 
-module.exports = {
-    input: './src/index.js',
-    output: {
-        file: 'dist/calio.min.js',
-        format: 'iife',
-        name: 'Calio',
-        sourcemap: true
+module.exports = [
+    {
+        input: 'src/index.js',
+        output: {
+            file: 'dist/calio.js',
+            format: 'esm'
+        },
+        plugins: [
+            resolve(),
+            common(),
+            svelte(config)
+        ]
     },
-    plugins: [
-        resolve(),
-        common(),
-        svelte(config)
-    ]
-};
+    {
+        input: 'src/index.js',
+        output: {
+            file: 'dist/calio.min.js',
+            format: 'iife',
+            name: 'Calio',
+            sourcemap: true
+        },
+        plugins: [
+            resolve(),
+            common(),
+            svelte(config)
+        ]
+    }
+];
---
+++ src/util.js
@@ -0,0 +1,87 @@
+export function makeDates(view, disabled) {
+    let current = view.clone().startOfMonth(),
+        dates = [],
+        dayOfFirst,
+        dayOfLast;
+
+    if (!Array.isArray(disabled)) {
+        return [];
+    }
+
+    dayOfFirst = current.dayOfWeek();
+
+    for (let i = 0; i < dayOfFirst; i++) {
+        dates.unshift(current.clone().date(-i));
+    }
+
+    current.endOfMonth();
+
+    for (let i = 1, days = current.date(); i <= days; i++) {
+        dates.push(current.clone().date(i));
+    }
+
+    dayOfLast = current.dayOfWeek();
+    current.startOfMonth().addMonth();
+
+    for (let i = 1; i < (7 - dayOfLast); i++) {
+        dates.push(current.clone().date(i));
+    }
+
+    return dates;
+}
+
+export function updateRange(day, current, strict, disabled) {
+    let selection = current || [],
+        index = selection.findIndex(s => s.isSame(day));
+
+    if (index > -1) {
+        selection.splice(index, 1);
+
+        return selection;
+    } else if (selection.length > 1) {
+        return [day.clone()];
+    }
+
+    selection = [...selection, day.clone()];
+
+    selection = selection.sort((a, b) => {
+        return a.timestamp() - b.timestamp();
+    });
+
+    if (strict) {
+        let [start, end] = selection,
+            isInvalid = end && !!disabled.find(d => {
+                return d.isAfter(start) && d.isBefore(end);
+            });
+
+        if (isInvalid) {
+            return;
+        }
+    }
+
+    return selection;
+
+}
+
+export function updateMulti(day, current, limit) {
+    let selection = current || [],
+        index = selection.findIndex(s => s.isSame(day));
+
+    if (index > -1) {
+        selection.splice(index, 1);
+
+        return selection;
+    } else if (!limit || selection.length < limit) {
+        return [...selection, day.clone()].sort((a, b) => {
+            return a.timestamp() - b.timestamp();
+        });
+    }
+}
+
+export function updateSingle(day, view) {
+    if (!view.isSameMonth(day)) {
+        view = day.clone().startOfMonth();
+    }
+
+    return day.clone();
+}
